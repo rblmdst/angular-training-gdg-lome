234bf019d29fde22830dc4de479fb7d71677d560
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2023-10-28 10:21:34 +0200

    [day-3] clean

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,32 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Njarasoa';
-  age = 32;
-  count = 2;
-  tempName = 'N/A';
-
-  mycolor = '';
-
-  inputType = 'password';
-
-  increment() {
-    this.count++;
-  }
-  decrement() {
-    this.count--;
-  }
-
-  nameChanghed(name: string) {
-    this.tempName = name;
-  }
-
-  toggle() {
-    if (this.inputType === 'password') {
-      this.inputType = 'text';
-    } else {
-      this.inputType = 'password';
-    }
-  }
-}
+export class AppComponent {}
